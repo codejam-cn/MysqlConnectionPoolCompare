7c3f58ee589291666ef24a6be3e19f1a17bca7b6
zhaos-lenovo <someone000@126.com>
2019-04-23 22:53:16 +0800

    添加项目文件。

---
+++ MysqlConnectionPoolCompare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MysqlConnectionPoolCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
